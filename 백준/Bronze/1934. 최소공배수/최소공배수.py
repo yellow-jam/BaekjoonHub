81141cf5d6338126b63d198864ccdf648df21db4
yellow-jam <88462774+yellow-jam@users.noreply.github.com>
2025-11-15 12:21:13 +0900

    [Bronze I] Title: 최소공배수, Time: 88 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -0,0 +1,14 @@
+t = int(input())
+
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a%b)
+
+
+for i in range(t):
+    a, b = map(int, input().split())
+    result = a*b // gcd(a, b)
+    print(result)
