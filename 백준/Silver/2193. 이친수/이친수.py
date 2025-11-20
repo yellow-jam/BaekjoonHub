f0d1f3d7deba67b6de5742c614874eed2c534e08
yellow-jam <88462774+yellow-jam@users.noreply.github.com>
2025-11-20 17:45:24 +0900

    [Silver III] Title: 이친수, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+D = [[0 for j in range(2)] for i in range(N+1)]
+
+D[1][1] = 1  # 1  -> 이친수
+D[1][0] = 0  # 0  -> 이친수가 아님 (0으로 시작하면 안됨)
+
+for i in range(2, N+1):
+    D[i][0] = D[i-1][0] + D[i-1][1]
+    D[i][1] = D[i-1][0]
+
+print(D[N][0] + D[N][1])
